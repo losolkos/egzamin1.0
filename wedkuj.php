6385b3728423cd09e19a9d2c4343ed814baa1b6e
losolkos <zelota23@wp.pl>
2023-09-18 11:24:31 +0200

    egzamin próba nr1

---
+++ wedkuj.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Wędkowanie</title>
+</head>
+
+<link rel="stylesheet" type="text/css" href="styl_1.css">
+<body>
+<div class="tytuł">
+    <h1 class="tekst">Portal dla wędkarzy</h1>
+</div>
+<div class ="lista">
+    <h2>Ryby zamieszkujące rzeki</h2>
+    <li>
+        1.szczupak pływa w rzece Warta-Obrzycko, Wielkopolskie
+</li>
+<li>
+        2.Leszcz pływa w rzece Prezemsza k. okradzionwa Siaskie
+    </li>
+</div>
+<div class= "tabela">
+    <h2>Ryby drapieżne naszych wód</h2>
+    <table class="tab">
+        <tr>
+            <th>L.p.</th>
+            <th>Gatunek</th>
+            <th>Występowanie</th>
+        </tr>
+        
+        <tr>
+            <td>1</td>
+            <td>szczupak</td>
+            <td>stawy, rzeki</td>
+        </tr>
+        
+        <tr>
+            <td>3</td>
+            <td>sandacz</td>
+            <td>stawy, jeziora, rzeki</td>
+        </tr>
+    
+        <tr>
+            <td>4</td>
+            <td>okon</td>
+            <td>rzeki</td>
+        </tr>
+        
+        <tr>
+            <td>5</td>
+            <td>sum </td>
+            <td>jeziora, rzeki</td>
+        </tr>
+        
+        <tr>
+            <td>6</td>
+            <td>dorsz</td>
+            <td>morza, oceany</td>
+        </tr>
+    </table>
+</div>
+
+<div class="zdjęcie" >
+<img src="../egzamin1/zdjęcia/ryba1.jpg" style="box-shadow: 10px 10px 20px dimgray"
+alt="sum" width="400px"><br><br>
+<a href="kwerendy.txt">pobierz kwerendy</a>
+</div>
+
+<div class ="tytul">
+    <h2 class = "tekst">Strone wykonał: Aleksander Gospodarczyk</h2>
+</div>
+</body>
+</html>
